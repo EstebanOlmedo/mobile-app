bcba4c67d18fe825b53a0e436ddf69b36a305903
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2023-02-15 20:26:39 -0600

    Change initial screen from Login to Splash

--- EVASEAC/app/App.navigator.tsx
+++ EVASEAC/app/App.navigator.tsx
@@ -7,17 +7,17 @@ const { Navigator, Screen } = createNativeStackNavigator();
 import HomeScreen from "./screens/HomeScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import LoginScreen from "./screens/LoginScreen";
+import SplashScreen from "./screens/SplashScreen";
 
 const AppNavigator = () => (
     <NavigationContainer>
-        <Navigator 
-            screenOptions={{
-                headerShown : false
-            }} initialRouteName="login"
+        <Navigator
+            initialRouteName="splash"
         >
-            <Screen name="login" component={LoginScreen}></Screen>
+            <Screen name="splash" component={SplashScreen} options={{ headerShown : false }}></Screen>
+            <Screen name="login" component={LoginScreen} options={{ headerShown : false }}></Screen>
             <Screen name="home" component={HomeScreen}></Screen>
-            <Screen name="signup" component={SignUpScreen}></Screen>
+            <Screen name="signup" component={SignUpScreen} options={{ title: 'Registro' }}></Screen>
         </Navigator>
     </NavigationContainer>
 );
